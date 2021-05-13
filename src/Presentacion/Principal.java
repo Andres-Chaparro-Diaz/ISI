5bc5b78a67d15c5898c1745e41b7c9a6d0ddc6da
Claudio-Nahuel-Diaz-Avalos <72039901+Claudio-Nahuel-Diaz-Avalos@users.noreply.github.com>
2021-05-13 18:43:02 +0200

    una tonteria

--- src/Presentacion/Principal.java
+++ src/Presentacion/Principal.java
@@ -61,7 +61,7 @@ public class Principal {
 		frameApp.setResizable(false);
 		frameApp.setIconImage(
 				Toolkit.getDefaultToolkit().getImage(Principal.class.getResource("/Recursos/moodapp.png")));
-		frameApp.setBounds(100, 100, 900, 650);
+		frameApp.setBounds(300, 100, 900, 650);
 		frameApp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frameApp.getContentPane().setLayout(new CardLayout(0, 0));
 		frameApp.getContentPane().add(login);
